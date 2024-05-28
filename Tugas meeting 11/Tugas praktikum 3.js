91bad6e78c0b8b22a30cd6125a3443ac41e5fece
psribuezraa <149931877+psribuezraa@users.noreply.github.com>
2024-05-29 06:55:44 +0700

    Add files via upload

---
+++ Tugas meeting 11/Tugas praktikum 3.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var n1 = document.getElementById("num1");
+    var n2 = document.getElementById("num2");
+    var res = document.getElementById("result");
+
+    document.getElementById("add").addEventListener("click", function() {
+        res.value = parseFloat(n1.value) + parseFloat(n2.value);
+    });
+
+    document.getElementById("sub").addEventListener("click", function() {
+        res.value = parseFloat(n1.value) - parseFloat(n2.value);
+    });
+
+    document.getElementById("mul").addEventListener("click", function() {
+        res.value = parseFloat(n1.value) * parseFloat(n2.value);
+    });
+
+    document.getElementById("div").addEventListener("click", function() {
+        res.value = parseFloat(n1.value) / parseFloat(n2.value);
+    });
+});
